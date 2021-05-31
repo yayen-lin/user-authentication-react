0660b47ddfdba786436d88e942ff52eef2b86d40
Andy Lin <lin383@wisc.edu>
2021-05-31 15:21:13 +0800

    re-structured project folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Howdy!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
