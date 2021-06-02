cd669513f44ceb1c573e65c60b7ced18fd01bee3
Chiwen.L <zxcvbnm840508@gmail.com>
2021-06-01 23:43:51 -0700

    modified title

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from the bootymaster!" });
 });
 
 app.listen(PORT, () => {
