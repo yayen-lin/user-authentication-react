2ac7397a11f873664aa225f1a58b2d30b75aaa00
Andy Lin <lin383@wisc.edu>
2021-06-03 14:19:01 +0800

    installed dotenv package

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const path = require("path");
-
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 console.log("PORT=", process.env.PORT || "undefined");
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+const db = mysql.createPool({
+  host: "162.241.253.180",
+  user: "ggdabhmy",
+  password: process.env.DB_PASSWORD,
+  database: "ggdabhmy_sampledb",
+  connectionLimit: 5,
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server! - MOJO" });
