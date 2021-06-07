f53706714a279ea92c270afecbe61b5d51ff32b7
Andy Lin <lin383@wisc.edu>
2021-06-07 16:08:59 +0800

    modified App.js and installed react-router-dom package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,20 @@
-import React, { useState, useEffect } from "react";
-import Axios from "axios";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+// import { Provider } from "react-redux";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect,
+// } from "react-router-dom";
+
+// import Axios from "axios";
+// import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const [data, setData] = useState(null);
-  const [movie, setMovie] = useState(null);
-  const [review, setReview] = useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
-  const temp = (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
-
-  const submitReview = () => {
-    Axios.post("http://localhost:3001/api/insert", {
-      movieName: movie,
-      movieReview: review,
-    }).then(() => {
-      alert("Insert Successfully!");
-    });
-  };
-
-  return (
-    <div className="App">
-      <h1>CRUD Practice</h1>
-      <div className="form">
-        <label>Movie Name:</label>
-        <input
-          type="text"
-          name="movieName"
-          onChange={(e) => {
-            setMovie(e.target.value);
-          }}
-        />
-        <label>Movie Review:</label>
-        <input
-          type="text"
-          name="movieReview"
-          onChange={(e) => {
-            setReview(e.target.value);
-          }}
-        />
-
-        <button onClick={submitReview}>Submit</button>
-      </div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
 }
 
 export default App;
