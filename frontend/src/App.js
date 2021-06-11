3596063de15f89db7b23223d5bcd0d8c881dd1f6
Andy Lin <lin383@wisc.edu>
2021-06-11 16:30:23 +0800

    added Navigation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
 // import { Provider } from "react-redux";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Redirect,
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 // import Axios from "axios";
 // import logo from "./logo.svg";
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Navigation from "./components/Navigation";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dummy_data: "abc",
+    };
+  }
+
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <Container>
+        <Router>
+          <Navigation />
+        </Router>
+        <h1>Hello World!</h1>
+      </Container>
+    );
   }
 }
 
