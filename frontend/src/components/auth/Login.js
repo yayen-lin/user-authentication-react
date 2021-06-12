7eaadbe7f035a5143dbc18674729f10606460cc1
Andy Lin <lin383@wisc.edu>
2021-06-12 23:11:26 +0800

    modified views for login page.

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+
+import { BiKey, BiLogIn } from "react-icons/bi";
 
 class Login extends Component {
   constructor(props) {
@@ -13,24 +19,58 @@ class Login extends Component {
 
   getLoginForm() {
     return (
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <Form className="mt-5">
+        {/* username */}
+        <Row>
+          <Col xs="auto">
+            <Form.Label>Username</Form.Label>
+            <InputGroup className="mb-2">
+              <InputGroup.Prepend>
+                <InputGroup.Text>@</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                id="username-form"
+                type="text"
+                placeholder="Username"
+              />
+            </InputGroup>
+          </Col>
+        </Row>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+        {/* password */}
+        <Row className="mt-2">
+          <Col xs="auto">
+            <Form.Label>Password</Form.Label>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>
+                  <BiKey />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                id="form-password"
+                type="password"
+                placeholder="Password"
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+
+        {/* remember me */}
+        <Row className="mt-2">
+          <Col xs="auto">
+            <Form.Check
+              type="checkbox"
+              id="autoSizingCheck"
+              className="mb-2"
+              label="Remember me"
+            />
+          </Col>
+        </Row>
+
+        {/* login btn */}
+        <Button variant="dark" size="lg" className="mt-3">
+          <BiLogIn /> Login
         </Button>
       </Form>
     );
