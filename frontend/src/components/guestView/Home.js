ab9a7b16850b4ad4b58c01db963b6370e7600e49
nickwu85 <nick860805@gmail.com>
2021-06-22 14:14:58 +0800

    modified home page

--- frontend/src/components/guestView/Home.js
+++ frontend/src/components/guestView/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <h1>Hello from the home page!</h1>;
+    return <h1>Hello 69from the home page!</h1>;
   }
 }
 
