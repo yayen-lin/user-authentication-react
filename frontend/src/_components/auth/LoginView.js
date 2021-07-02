f2abaa37521c37120119a44bd71119e3b2698eb8
Andy Lin <lin383@wisc.edu>
2021-07-02 11:29:16 +0800

    added signup and login views and scss files

--- frontend/src/_components/auth/LoginView.js
+++ frontend/src/_components/auth/LoginView.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import InputGroup from "react-bootstrap/InputGroup";
+
 // imports for
 import { MDBInput, MDBBtn, MDBCheckbox } from "mdb-react-ui-kit";
+
+import "./login.scss";
 
 function LoginView() {
   const [formValue, setFormValue] = useState({
@@ -11,6 +18,82 @@ function LoginView() {
     username: "",
     email: "",
   });
+  // States for the form
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [errors, setErrors] = useState({});
+
+  /**
+   * handle validation on form submit
+   *
+   * @param {*} e - event
+   */
+  const handleSubmit = async (e) => {
+    if (e) {
+      e.preventDefault();
+    }
+
+    let allErrors = null;
+    // validation for number
+    if (
+      quantity < 0 || // must be positive
+      quantity === null || // not bu null
+      !Number.isInteger(parseInt(quantity)) // is a number
+    ) {
+      allErrors = {};
+      allErrors.quantity = "Please provide a valid quantity number.";
+    }
+    // validation for name
+    if (name.length < 1 || name === null) {
+      allErrors = {};
+      allErrors.name = "Please provide a name for the item.";
+    }
+
+    if (allErrors) {
+      setErrors(allErrors);
+      return;
+    }
+    // addItem();
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Form.Row className="mt-4">
+          <Form.Group className="floating-label-group" as={Col}>
+            <Form.Control
+              required
+              id="item-name"
+              type="text"
+              value={name}
+              isInvalid={!!errors.name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Form.Label className="floating-label">Name</Form.Label>
+            <Form.Control.Feedback type="invalid">
+              {errors.name}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group as={Col}>
+            <Form.Label>Quantity</Form.Label>
+            <Form.Control
+              required
+              id="item-quantity"
+              type="number"
+              placeholder="quantity (i.e. a number)"
+              value={quantity}
+              isInvalid={!!errors.quantity} // used for form control feedback to validate
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.quantity}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+      </Form>
+    </Container>
+  );
 }
 
 export default LoginView;
--- frontend/src/_components/auth/SignupView.js
+++ frontend/src/_components/auth/SignupView.js
@@ -16,6 +16,9 @@ import {
   MDBIcon,
 } from "mdb-react-ui-kit";
 
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 import "./auth.scss";
@@ -30,7 +33,7 @@ function SignupView(props) {
     confirmedPassword: "",
   });
 
-  const [error, setError] = useState(true);
+  const [errors, setErrors] = useState({});
 
   // setting validation message
   const [validateLN, setValidateLN] = useState("請輸入您的姓氏"); // lastname
@@ -39,10 +42,10 @@ function SignupView(props) {
   const [validateEmail, setValidateEmail] = useState("請輸入您的電子郵件"); // email
   const [validatePW, setValidatePW] = useState("請輸入您想要設定的密碼"); // password
   const [validateCPW, setValidateCPW] = useState("請再次輸入您的密碼"); // confirmed password
-  const [validateCB, setValidateCB] = useState("請點選接受然後註冊"); // checkbox
 
   const onChange = (e) => {
-    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+    // trim extra whitespace
+    setFormValue({ ...formValue, [e.target.name]: e.target.value.trim() });
   };
 
   // in company email
@@ -63,6 +66,32 @@ function SignupView(props) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    let allErrors = {};
+
+    if (!formValue.lname) {
+      allErrors.lname_error = "請輸入您的姓氏";
+    }
+
+    if (!formValue.fname) {
+      allErrors.fname_error = "請輸入您的名字";
+    }
+
+    if (!formValue.username) {
+      allErrors.username_error = "請輸入您的用戶名";
+    }
+
+    if (!formValue.email) {
+      allErrors.email_error = "請輸入您的電子郵件";
+    }
+
+    if (!formValue.password) {
+      allErrors.password_error = "請輸入您想要設定的密碼";
+    }
+
+    if (!formValue.confirmedPassword) {
+      allErrors.confirmedPassword_error = "請再次輸入您的密碼";
+    }
+
     const user = {
       lname: formValue.lname,
       fname: formValue.fname,
@@ -75,18 +104,12 @@ function SignupView(props) {
 
     console.log(user);
 
-    if (
-      !formValue.lname ||
-      !formValue.fname ||
-      !formValue.username ||
-      !formValue.email ||
-      !formValue.password ||
-      !formValue.confirmedPassword
-    ) {
-      setError(true);
-    }
-    if (!error) {
-      setError(false);
+    if (allErrors) {
+      setErrors(allErrors);
+      console.log(allErrors);
+      return;
+    } else {
+      // call signup
       // let signupResult = await this.props.signup(user);
       // clear fields upon successful signup
       // clearSignupFields();
@@ -98,182 +121,164 @@ function SignupView(props) {
     <MDBContainer className="my-4 justify-content-center">
       <h1>註冊</h1>
       <hr />
-      <MDBValidation
+      <Form
+        onSubmit={(e) => handleSubmit(e)}
         className="row g-3"
-        noValidate // calls off browser's default validation and use our customized one
+        noValidate // calls off browser's default validation and use our customized one>
       >
-        {/* lastname */}
-        <MDBRow>
-          <MDBCol lg="4" className="position-relative mt-4">
-            {/* <MDBInput
-              validation={validateLN}
-              validationTooltip
-              label="姓"
-              id="validationTooltip-lname"
+        <Form.Row>
+          {/* lastname */}
+          <Form.Group
+            as={Col}
+            xl="4"
+            className="floating-label-group has-validation form-outline mt-4"
+          >
+            <Form.Control
+              type="text"
+              id="validate-lname"
               name="lname"
               value={formValue.lname}
               onChange={onChange}
+              autoComplete="off"
+              isInvalid={!!errors.lname}
               required
-              invalid
-            /> */}
-            <div class="floating-label-group has-validation">
-              <input
-                type="text"
-                id="validate-lname"
-                class="form-control"
-                name="lname"
-                value={formValue.lname}
-                onChange={onChange}
-                required
-              />
-              <label class="floating-label">姓</label>
-              <div class="invalid-tooltip">{validateLN}</div>
-            </div>
-          </MDBCol>
+            />
+            <Form.Control.Feedback type="invalid" tooltip>
+              {errors.lname}
+            </Form.Control.Feedback>
+            <Form.Label class="floating-label">姓</Form.Label>
+          </Form.Group>
 
           {/* firstname */}
-          <MDBCol lg="4" className="position-relative mt-4">
-            <MDBInput
-              validation={validateFN}
-              validationTooltip
-              label="名"
-              id="validationTooltip-fname"
+          <Form.Group
+            as={Col}
+            xl="4"
+            className="floating-label-group has-validation form-outline mt-4"
+          >
+            <Form.Control
+              type="text"
+              id="validate-fname"
               name="fname"
               value={formValue.fname}
               onChange={onChange}
+              autoComplete="off"
+              isInvalid={!!errors.fname}
               required
-              invalid
             />
-          </MDBCol>
-        </MDBRow>
+            <Form.Label class="floating-label">名</Form.Label>
+            <Form.Control.Feedback type="invalid" tooltip>
+              {errors.fname}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
 
         {/* username */}
         <MDBRow className="position-relative">
-          <MDBCol lg="4" className="position-relative mt-4">
-            <div className="input-group has-validation form-outline">
-              <span
-                className="input-group-text"
-                id="validationTooltipUsernamePrepend"
-              >
-                <MDBIcon className="ms-1" icon="at" size="1.5x" />
-              </span>
+          <MDBCol xl="4" className="position-relative mt-4">
+            <div className="floating-label-group has-validation form-outline input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="username-prepend-icon">
+                  <MDBIcon className="ms-1" icon="at" size="1.5x" />
+                </span>
+              </div>
               <input
                 type="text"
+                id="validation-username"
                 class="form-control"
-                id="validationTooltip-username"
-                aria-describedby="validationTooltipUsernamePrepend"
                 name="username"
                 value={formValue.username}
                 onChange={onChange}
+                autoComplete="off"
                 required
               />
-              <label
-                htmlFor="validationTooltip-username"
-                class="form-label"
-                style={{ marginLeft: "3em" }}
-              >
-                用戶名
-              </label>
+
+              <label class="floating-label-w-icon">用戶名</label>
               <div className="invalid-tooltip">{validateUN}</div>
-              <div class="form-notch">
-                <div class="form-notch-leading" style={{ width: "70px" }}></div>
-                <div class="form-notch-middle" style={{ width: "60px" }}></div>
-                <div class="form-notch-trailing"></div>
-              </div>
             </div>
-            {/* </div> */}
-          </MDBCol>
-          <MDBCol lg="4" className="position-relative mt-4">
+
             {/* email */}
             {/* TODO: border overlapped for some reasons */}
-            {/* <div className="col-12 col-md-6 position-relative"> */}
-            <div className="input-group has-validation form-outline">
-              {/* prepend element */}
-              <span className="input-group-text" id="inputGroupPrepend">
-                <MDBIcon className="ms-1" icon="envelope-square" size="lg" />
-              </span>
+          </MDBCol>
+          <MDBCol xl="4" className="position-relative mt-4">
+            <div className="floating-label-group has-validation form-outline input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="email-prepend-icon">
+                  <MDBIcon
+                    className="ms-1"
+                    icon="envelope-square"
+                    size="1.5x"
+                  />
+                </span>
+              </div>
               <input
                 type="text"
+                id="validation-email"
                 className="form-control"
-                id="validationTooltip-email"
                 name="email"
                 value={formValue.email}
                 onChange={onChange}
+                autoComplete="off"
                 required
               />
-              <label
-                for="validationTooltipUsername"
-                class="form-label"
-                style={{ marginLeft: "3em" }}
-              >
-                電子郵件
-              </label>
+              <label class="floating-label-w-icon">電子郵件</label>
+              <div class="input-group-append">
+                <span
+                  class="input-group-text"
+                  id="email-append"
+                  style={{ paddingLeft: "0px" }}
+                >
+                  <strong style={{ fontSize: "0.8rem" }}>@carmax168.com</strong>
+                </span>
+              </div>
 
-              {/* append element */}
-              <span className="input-group-text" id="inputGroupAppend">
-                <strong style={{ fontSize: "0.8rem" }}>@carmax168.com</strong>
-              </span>
               {/* error message */}
               <div className="invalid-tooltip">{validateEmail}</div>
-
-              <div class="form-notch">
-                <div class="form-notch-leading" style={{ width: "70px" }}></div>
-                <div class="form-notch-middle" style={{ width: "80px" }}></div>
-                <div class="form-notch-trailing"></div>
-              </div>
             </div>
-            {/* </div> */}
           </MDBCol>
         </MDBRow>
 
         <MDBRow>
           {/* password */}
-          <MDBCol lg="4" className="position-relative mt-4">
-            <MDBInput
-              validation={validatePW}
-              validationTooltip
-              label="密碼"
-              id="validationTooltip-password"
-              name="password"
-              value={formValue.password}
-              onChange={onChange}
-              type="password"
-              required
-              invalid
-            />
+          <MDBCol xl="4" className="position-relative mt-4">
+            <div className="floating-label-group has-validation form-outline">
+              <input
+                type="password"
+                id="validation-password"
+                className="form-control"
+                name="password"
+                value={formValue.password}
+                onChange={onChange}
+                autoComplete="off"
+                required
+              />
+              <label class="floating-label">密碼</label>
+
+              {/* error message */}
+              <div className="invalid-tooltip">{validatePW}</div>
+            </div>
           </MDBCol>
 
           {/* confirmed password */}
-          <MDBCol lg="4" className="position-relative mt-4">
-            <MDBInput
-              validation={validateCPW}
-              validationTooltip
-              label="確認密碼"
-              id="validationTooltip-confirmedPassword"
-              name="confirmedPassword"
-              value={formValue.confirmedPassword}
-              onChange={onChange}
-              type="password"
-              required
-              invalid
-            />
+          <MDBCol xl="4" className="position-relative mt-4">
+            <div className="floating-label-group has-validation form-outline">
+              <input
+                type="password"
+                id="validation-confirmedPassword"
+                className="form-control"
+                name="confirmedPassword"
+                value={formValue.confirmedPassword}
+                onChange={onChange}
+                autoComplete="off"
+                required
+              />
+              <label class="floating-label">確認密碼</label>
+              {/* error message */}
+              <div className="invalid-tooltip">{validatePW}</div>
+            </div>
           </MDBCol>
         </MDBRow>
 
-        {/* <input
-              type="text"
-              className="form-control"
-              id="validationTooltip-username"
-              placeholder="用戶名"
-              name="username"
-              value={formValue.username}
-              onChange={onChange}
-              required
-            />
-            <div className="invalid-tooltip">{validateUN}</div> */}
-
-        <div className="col-12 position-relative">
-          {/* TODO: Add a modal that shows terms and conditions */}
+        {/* <div className="col-12 position-relative">
           <MDBCheckbox
             validation={validateCB}
             validationTooltip
@@ -282,7 +287,7 @@ function SignupView(props) {
             required
             invalid
           />
-        </div>
+        </div> */}
 
         {/* submit btn */}
         <div className="col-12 pt-2 position-relative">
@@ -297,7 +302,7 @@ function SignupView(props) {
             註冊！
           </MDBBtn>
         </div>
-      </MDBValidation>
+      </Form>
     </MDBContainer>
   );
 }
